79e4cc212be006beb559b2dec20d8ce10f67eb6e
CristanJosh <127463324+CristanJosh@users.noreply.github.com>
2024-12-06 00:43:19 +0800

    Update profile.blade.php

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,3 +1,10 @@
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+@livewireStyles
+@livewireScripts
+<script src="//unpkg.com/alpinejs" defer></script>
+<script src="https://cdn.tailwindcss.com"></script>
+<link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
