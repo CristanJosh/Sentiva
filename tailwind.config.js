7f1240b90fb6696cdf15a17ef29e9ee746cc7d92
CristanJosh <127463324+CristanJosh@users.noreply.github.com>
2024-12-05 23:19:11 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,16 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./layouts/**/*.{js,ts,jsx,tsx}",
     ],
+    
 
     theme: {
         extend: {
@@ -19,3 +23,4 @@ export default {
 
     plugins: [forms],
 };
+
